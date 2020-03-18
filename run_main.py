ffffb16d200d7156f5a3bc3dbf5691bdb0f4f699
hongming.tang <17792659770@163.com>
2020-03-18 19:53:57 +0800

    加入工具类，并且调整项目结构

--- run_main.py
+++ run_main.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from case.register_case import RegisterCase
+import sys
 
 if __name__ == '__main__':
+    print(sys.path)
     RegisterCase().main()
--- util/path_util.py
+++ util/path_util.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import sys
 import os
 
 
@@ -10,21 +9,8 @@ class PathUtil:
         """
             获取项目根目录
         """
-        # 判断调试模式
-        debug_vars = dict((a, b) for a, b in os.environ.items() if a.find('IPYTHONENABLE') >= 0)
-        # 根据不同场景获取根目录
-        if len(debug_vars) > 0:
-            # 当前为debug运行时
-            rootPath = sys.path[2]
-        elif getattr(sys, 'frozen', False):
-            # 当前为exe运行时
-            rootPath = os.getcwd()
-        else:
-            # 正常执行
-            rootPath = sys.path[1]
-        # 替换斜杠
-        rootPath = rootPath.replace("\\", "/")
-        return rootPath
+        root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        return root_path
 
     @staticmethod
     def get_file_path(abs_file_path):
