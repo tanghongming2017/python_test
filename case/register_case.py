631c1f1a8b8b9f3b5bee9e0f582293d45531acaa
hongming.tang <17792659770@163.com>
2020-03-19 12:58:19 +0800

    加入对chrome在服务器中的配置

--- case/register_case.py
+++ case/register_case.py
@@ -28,7 +28,7 @@ class RegisterCase(unittest.TestCase):
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.get("http://www.yundama.com/index/reg")
         self.driver.maximize_window()
